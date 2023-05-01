48e4f41fe45d7297088427e8b99220f4b511616d
drewminns <dminns@gmail.com>
2023-04-30 22:41:05 -0400

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+
+  React.useEffect(() => {
+
+  })
+  
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# solarleadfactory
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-g92j4w)
